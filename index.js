a12f8c9a215d760c62e4ff53d64f4b535348b0c4
Shaked Ashkenazi <shaked.a21@gmail.com>
2019-06-17 16:15:54 +0300

    Added config file & fixed documentation

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const exec = require('child_process').exec;
 const notifier = require('node-notifier');
+const config = require('./config/config');
 
-const cmd = 'dnf check-update'  // Shell command - Check for updates
-const title = 'Update Info';    // Title of notification
+const cmd = 'dnf check-update'       // Shell command - Check for updates
+const title = config.notifTitle;     // Title of notification
 
 /*
-Setting exit codes according to dnf manual,
+DNF exit codes for check-update.
 " DNF exit code will be 100
  when there are updates available and a list of the updates will be printed,
  0 if not and 1 if an error occurs."
@@ -19,33 +20,37 @@ const ExitCode = {
   ERROR: 1
 }
 
+
 exec(cmd, (err, stdout, stderr) => {
   console.log("Checking for updates..");
 
+  /* Catching error.
+      Not breaking because it might be a false-positive in case of ExitCode.
+      AVAILABLE.
+  */
   if (err) {
     console.error(`exec error: ${err}`);
-    return;
   }
 
-  console.log('-------- Debug ----------')
   console.log(`stdout: ${stdout}`);
   console.log(`stderr: ${stderr}`);
 
 
 }).on('exit', (code) => {
+  // Using exit codes to determine whether updates are available or not.
   console.log(`exit code: ${code}`);
 
   switch(code) {
     case ExitCode.AVAILABLE:
       notifier.notify({
         title: title,
-        message: 'Updates are available'
+        message: config.updateMessage
       });
       break;
     case ExitCode.ERROR:
       notifier.notify({
         title: title,
-        message: 'An error occurred while checking for updates'
+        message: config.errorMessage
       });
       break;
   }
